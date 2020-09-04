edb4ae655a040345c9866562c12a94f122829b09
nmagrogan <nmagrogan@zagmail.gonzaga.edu>
2020-09-03 17:25:45 -0700

    added basic html and css to get layout of calculator, javascript not connected yet

--- calculator.js
+++ calculator.js
@@ -6,30 +6,11 @@ function subtract (num1,num2) {
 	return num1-num2;
 }
 
-function sum (nums) {
-	let sum = 0;
-	for(i=0;i < nums.length; i++){
-		sum += nums[i];
-	}
-	return sum;
+
+function multiply (num1, num2) {
+	return num1*num2;
 }
 
-function multiply (nums) {
-	let multiple = 1;
-	for(i=0; i<nums.length;i++){
-		multiple *= nums[i];
-	}
-	return multiple
-}
-
-function power(num1, num2) {
-	return num1**num2;
-}
-
-function factorial(num) {
-	fact = 1;
-	for(i = num; i >0; i--){
-		fact *= i;
-	}
-	return fact
+function divide(num1,num2){
+  return num1/num2;
 }
